56475ad6ceb0615419fc67ccab57c48b3512e26a
nab138 <nab@nabdev.me>
2024-12-07 00:50:19 -0500

    I have no clue if this is correct

--- src/main/java/frc/robot/constants/DriveTrainConstants.java
+++ src/main/java/frc/robot/constants/DriveTrainConstants.java
@@ -14,93 +14,99 @@ import org.ironmaple.simulation.drivesims.GyroSimulation;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 /**
- * stores the constants and PID configs for chassis because we want an all-real simulation for the chassis, the numbers
+ * stores the constants and PID configs for chassis because we want an all-real
+ * simulation for the chassis, the numbers
  * are required to be considerably precise
  */
 public class DriveTrainConstants {
-    /** numbers that needs to be changed to fit each robot TODO: change these numbers to match your robot */
-    public static final double WHEEL_COEFFICIENT_OF_FRICTION = 1.43;
+        /**
+         * numbers that needs to be changed to fit each robot TODO: change these numbers
+         * to match your robot
+         */
+        public static final double WHEEL_COEFFICIENT_OF_FRICTION = 1.43;
 
-    public static final Mass ROBOT_MASS = Kilograms.of(45); // robot weight with bumpers
+        public static final Mass ROBOT_MASS = Kilograms.of(45); // robot weight with bumpers
 
-    /** TODO: change motor type to match your robot */
-    public static final DCMotor DRIVE_MOTOR = DCMotor.getNEO(1);
+        /** TODO: change motor type to match your robot */
+        public static final DCMotor DRIVE_MOTOR = DCMotor.getNEO(1);
 
-    public static final DCMotor STEER_MOTOR = DCMotor.getNeo550(1);
+        public static final DCMotor STEER_MOTOR = DCMotor.getNeo550(1);
 
-    /** numbers imported from {@link TunerConstants} TODO: for REV chassis, replace them with actual numbers */
-    public static final Distance WHEEL_RADIUS = Inches.of(1.5);
+        /**
+         * numbers imported from {@link TunerConstants} TODO: for REV chassis, replace
+         * them with actual numbers
+         */
+        public static final Distance WHEEL_RADIUS = Inches.of(1.5);
 
-    public static final double DRIVE_GEAR_RATIO = 4.71;
-    public static final double STEER_GEAR_RATIO = 46.42;
+        public static final double DRIVE_GEAR_RATIO = 4.71;
+        public static final double STEER_GEAR_RATIO = 46.42;
 
-    public static final Voltage STEER_FRICTION_VOLTAGE = Volts.of(0.25);
-    public static final Voltage DRIVE_FRICTION_VOLTAGE = Volts.of(0.25);
-    public static final MomentOfInertia STEER_INERTIA = KilogramSquareMeters.of(0.025);
+        public static final Voltage STEER_FRICTION_VOLTAGE = Volts.of(0.25);
+        public static final Voltage DRIVE_FRICTION_VOLTAGE = Volts.of(0.25);
+        public static final MomentOfInertia STEER_INERTIA = KilogramSquareMeters.of(0.025);
 
-    /* adjust current limit */
-    public static final Current DRIVE_CURRENT_LIMIT = Amps.of(40);
-    public static final Current STEER_CURRENT_LIMIT = Amps.of(20);
-    
+        /* adjust current limit */
+        public static final Current DRIVE_CURRENT_LIMIT = Amps.of(40);
+        public static final Current STEER_CURRENT_LIMIT = Amps.of(20);
 
-    public static final Distance TRACK_WIDTH = Inches.of(26);
-    public static final Distance TRACK_LENGTH = Inches.of(26);
-    public static final double ROBOT_SIZE = Units.inchesToMeters(37);
+        public static final Distance TRACK_WIDTH = Inches.of(26);
+        public static final Distance TRACK_LENGTH = Inches.of(26);
+        public static final double ROBOT_SIZE = Units.inchesToMeters(37);
 
+        /** translations of the modules to the robot center, in FL, FR, BL, BR */
+        public static final Translation2d[] MODULE_TRANSLATIONS = new Translation2d[] {
+                        new Translation2d(-TRACK_WIDTH.in(Meters) / 2, TRACK_LENGTH.in(Meters) / 2),
+                        new Translation2d(TRACK_WIDTH.in(Meters) / 2, TRACK_LENGTH.in(Meters) / 2),
+                        new Translation2d(-TRACK_WIDTH.in(Meters) / 2, -TRACK_LENGTH.in(Meters) / 2),
+                        new Translation2d(TRACK_WIDTH.in(Meters) / 2, -TRACK_LENGTH.in(Meters) / 2)
+        };
 
-    /** translations of the modules to the robot center, in FL, FR, BL, BR */
-    public static final Translation2d[] MODULE_TRANSLATIONS = new Translation2d[] {
-        new Translation2d(-TRACK_WIDTH.in(Meters) / 2, TRACK_LENGTH.in(Meters) / 2),
-        new Translation2d(TRACK_WIDTH.in(Meters) / 2, TRACK_LENGTH.in(Meters) / 2),
-        new Translation2d(-TRACK_WIDTH.in(Meters) / 2, -TRACK_LENGTH.in(Meters) / 2),
-        new Translation2d(TRACK_WIDTH.in(Meters) / 2, -TRACK_LENGTH.in(Meters) / 2)
-    };
+        public static final double[] CHASSIS_ANGULAR_OFFSETS = { -Math.PI / 2, 0, Math.PI, Math.PI / 2 };
 
-    public static final double[] CHASSIS_ANGULAR_OFFSETS = {-Math.PI / 2, 0, Math.PI, Math.PI / 2};
+        /* equations that calculates some constants for the simulator (don't modify) */
+        private static final double GRAVITY_CONSTANT = 9.8;
 
-    /* equations that calculates some constants for the simulator (don't modify) */
-    private static final double GRAVITY_CONSTANT = 9.8;
+        public static final Distance DRIVE_BASE_RADIUS = Meters.of(MODULE_TRANSLATIONS[0].getNorm());
 
-    public static final Distance DRIVE_BASE_RADIUS = Meters.of(MODULE_TRANSLATIONS[0].getNorm());
+        /* friction_force = normal_force * coefficient_of_friction */
+        public static final LinearAcceleration MAX_FRICTION_ACCELERATION = MetersPerSecondPerSecond
+                        .of(GRAVITY_CONSTANT * WHEEL_COEFFICIENT_OF_FRICTION);
 
-    /* friction_force = normal_force * coefficient_of_friction */
-    public static final LinearAcceleration MAX_FRICTION_ACCELERATION =
-            MetersPerSecondPerSecond.of(GRAVITY_CONSTANT * WHEEL_COEFFICIENT_OF_FRICTION);
+        /* force = torque / distance */
+        public static final Force MAX_PROPELLING_FORCE = NewtonMeters.of(
+                        DRIVE_MOTOR.getTorque(DRIVE_CURRENT_LIMIT.in(Amps)) * DRIVE_GEAR_RATIO)
+                        .divide(WHEEL_RADIUS);
 
-    /* force = torque / distance */
-    public static final Force MAX_PROPELLING_FORCE = NewtonMeters.of(
-                    DRIVE_MOTOR.getTorque(DRIVE_CURRENT_LIMIT.in(Amps)) * DRIVE_GEAR_RATIO)
-            .divide(WHEEL_RADIUS);
+        /* floor_speed = wheel_angular_velocity * wheel_radius */
+        public static final LinearVelocity CHASSIS_MAX_VELOCITY = MetersPerSecond
+                        .of(DRIVE_MOTOR.freeSpeedRadPerSec / DRIVE_GEAR_RATIO * WHEEL_RADIUS.in(Meters));
+        public static final LinearAcceleration CHASSIS_MAX_ACCELERATION = (LinearAcceleration) Measure
+                        .min(MAX_FRICTION_ACCELERATION, MAX_PROPELLING_FORCE.divide(ROBOT_MASS));
+        public static final AngularVelocity CHASSIS_MAX_ANGULAR_VELOCITY = RadiansPerSecond
+                        .of(CHASSIS_MAX_VELOCITY.in(MetersPerSecond) / DRIVE_BASE_RADIUS.in(Meters));
+        public static final AngularAcceleration CHASSIS_MAX_ANGULAR_ACCELERATION = RadiansPerSecondPerSecond.of(
+                        CHASSIS_MAX_ACCELERATION.in(MetersPerSecondPerSecond) / DRIVE_BASE_RADIUS.in(Meters) * 2);
 
-    /* floor_speed = wheel_angular_velocity * wheel_radius */
-    public static final LinearVelocity CHASSIS_MAX_VELOCITY =
-            MetersPerSecond.of(DRIVE_MOTOR.freeSpeedRadPerSec / DRIVE_GEAR_RATIO * WHEEL_RADIUS.in(Meters));
-    public static final LinearAcceleration CHASSIS_MAX_ACCELERATION =
-            (LinearAcceleration) Measure.min(MAX_FRICTION_ACCELERATION, MAX_PROPELLING_FORCE.divide(ROBOT_MASS));
-    public static final AngularVelocity CHASSIS_MAX_ANGULAR_VELOCITY =
-            RadiansPerSecond.of(CHASSIS_MAX_VELOCITY.in(MetersPerSecond) / DRIVE_BASE_RADIUS.in(Meters));
-    public static final AngularAcceleration CHASSIS_MAX_ANGULAR_ACCELERATION = RadiansPerSecondPerSecond.of(
-            CHASSIS_MAX_ACCELERATION.in(MetersPerSecondPerSecond) / DRIVE_BASE_RADIUS.in(Meters) * 2);
+        public static final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(MODULE_TRANSLATIONS);
 
-    public static final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(MODULE_TRANSLATIONS);
+        /* for collision detection in simulation */
+        public static final Distance BUMPER_WIDTH = Inches.of(30), BUMPER_LENGTH = Inches.of(30);
 
-    /* for collision detection in simulation */
-    public static final Distance BUMPER_WIDTH = Inches.of(30), BUMPER_LENGTH = Inches.of(30);
+        // https://unacademy.com/content/upsc/study-material/physics/moment-of-inertia-of-rectangle-section/
+        public static final MomentOfInertia ROBOT_MOI = KilogramSquareMeters.of(ROBOT_MASS.in(Kilograms)
+                        * (BUMPER_WIDTH.in(Meters) * BUMPER_WIDTH.in(Meters)
+                                        + BUMPER_LENGTH.in(Meters) * BUMPER_LENGTH.in(Meters))
+                        / 12.0);
 
-    // https://unacademy.com/content/upsc/study-material/physics/moment-of-inertia-of-rectangle-section/
-    public static final MomentOfInertia ROBOT_MOI = KilogramSquareMeters.of(ROBOT_MASS.in(Kilograms)
-            * (BUMPER_WIDTH.in(Meters) * BUMPER_WIDTH.in(Meters) + BUMPER_LENGTH.in(Meters) * BUMPER_LENGTH.in(Meters))
-            / 12.0);
+        public static final Supplier<GyroSimulation> gyroSimulationFactory = GyroSimulation.getGeneric();
 
-    public static final Supplier<GyroSimulation> gyroSimulationFactory = GyroSimulation.getGeneric();
+        /* dead configs, don't change them */
+        public static final int ODOMETRY_CACHE_CAPACITY = 10;
+        public static final double ODOMETRY_FREQUENCY = 250;
+        public static final double ODOMETRY_WAIT_TIMEOUT_SECONDS = 0.02;
+        public static final int SIMULATION_TICKS_IN_1_PERIOD = 5;
 
-    /* dead configs, don't change them */
-    public static final int ODOMETRY_CACHE_CAPACITY = 10;
-    public static final double ODOMETRY_FREQUENCY = 250;
-    public static final double ODOMETRY_WAIT_TIMEOUT_SECONDS = 0.02;
-    public static final int SIMULATION_TICKS_IN_1_PERIOD = 5;
-
-public static final class ModuleConstants {
+        public static final class ModuleConstants {
                 // The MAXSwerve module can be configured with one of three pinion gears: 12T,
                 // 13T, or 14T.
                 // This changes the drive speed of the module (a pinion gear with more teeth
@@ -156,4 +162,7 @@ public static final class ModuleConstants {
                 public static final int kTurningMotorCurrentLimit = 20; // amps
         }
 
+        public static final class NeoMotorConstants {
+                public static final double kFreeSpeedRpm = 5676;
+        }
 }
--- src/main/java/frc/robot/subsystems/drive/IO/ModuleIOSpark.java
+++ src/main/java/frc/robot/subsystems/drive/IO/ModuleIOSpark.java
@@ -5,19 +5,14 @@
 
 package frc.robot.subsystems.drive.IO;
 
-import static edu.wpi.first.units.Units.Degrees;
 import static frc.robot.constants.DriveTrainConstants.*;
 
-import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.AlternateEncoderConfig.Type;
 import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
@@ -25,137 +20,129 @@ import edu.wpi.first.wpilibj.RobotController;
 import java.util.Queue;
 
 /**
- * Module IO implementation for SparkMax drive motor controller, SparkMax turn motor controller (NEO or NEO 550), and
+ * Module IO implementation for SparkMax drive motor controller, SparkMax turn
+ * motor controller (NEO or NEO 550), and
  * analog absolute encoder connected to the RIO
  *
- * <p>NOTE: This implementation should be used as a starting point and adapted to different hardware configurations
+ * <p>
+ * NOTE: This implementation should be used as a starting point and adapted to
+ * different hardware configurations
  * (e.g. If using a CANcoder, copy from "ModuleIOTalonFX")
  *
- * <p>To calibrate the absolute encoder offsets, point the modules straight (such that forward motion on the drive motor
- * will propel the robot forward) and copy the reported values from the absolute encoders using AdvantageScope. These
+ * <p>
+ * To calibrate the absolute encoder offsets, point the modules straight (such
+ * that forward motion on the drive motor
+ * will propel the robot forward) and copy the reported values from the absolute
+ * encoders using AdvantageScope. These
  * values are logged under "/Drive/ModuleX/TurnAbsolutePositionRad"
  */
 public class ModuleIOSpark implements ModuleIO {
-    private final SparkMax driveSparkMax;
-    private final SparkMax turnSparkMax;
+        private final SparkMax driveSparkMax;
+        private final SparkMax turnSparkMax;
 
-    private final RelativeEncoder drivingEncoder;
-    private final AbsoluteEncoder turningEncoder;
-    private final Queue<Double> drivePositionInput;
-    private final Queue<Double> steerRelativeEncoderPositionUngeared;
+        private final RelativeEncoder drivingEncoder;
+        private final AbsoluteEncoder turningEncoder;
+        private final Queue<Double> drivePositionInput;
+        private final Queue<Double> steerRelativeEncoderPositionUngeared;
 
-    private final boolean isTurnMotorInverted = true;
+        private final Rotation2d angularOffset;
 
-    public ModuleIOSpark(int index) {
-        switch (index) {
-            case 0 -> {
-                driveSparkMax = new SparkMax(17, SparkLowLevel.MotorType.kBrushless);
-                turnSparkMax = new SparkMax(18, SparkLowLevel.MotorType.kBrushless);
-            }
-            case 1 -> {
-                driveSparkMax = new SparkMax(15, SparkLowLevel.MotorType.kBrushless);
-                turnSparkMax = new SparkMax(16, SparkLowLevel.MotorType.kBrushless);
-            }
-            case 2 -> {
-                driveSparkMax = new SparkMax(12, SparkLowLevel.MotorType.kBrushless);
-                turnSparkMax = new SparkMax(11, SparkLowLevel.MotorType.kBrushless);
-            }
-            case 3 -> {
-                driveSparkMax = new SparkMax(13, SparkLowLevel.MotorType.kBrushless);
-                turnSparkMax = new SparkMax(14, SparkLowLevel.MotorType.kBrushless);
-            }
-            default -> throw new RuntimeException("Invalid module index");
+        private final boolean isTurnMotorInverted = true;
+
+        public ModuleIOSpark(int index) {
+                switch (index) {
+                        case 0 -> {
+                                driveSparkMax = new SparkMax(17, SparkLowLevel.MotorType.kBrushless);
+                                turnSparkMax = new SparkMax(18, SparkLowLevel.MotorType.kBrushless);
+                        }
+                        case 1 -> {
+                                driveSparkMax = new SparkMax(15, SparkLowLevel.MotorType.kBrushless);
+                                turnSparkMax = new SparkMax(16, SparkLowLevel.MotorType.kBrushless);
+                        }
+                        case 2 -> {
+                                driveSparkMax = new SparkMax(12, SparkLowLevel.MotorType.kBrushless);
+                                turnSparkMax = new SparkMax(11, SparkLowLevel.MotorType.kBrushless);
+                        }
+                        case 3 -> {
+                                driveSparkMax = new SparkMax(13, SparkLowLevel.MotorType.kBrushless);
+                                turnSparkMax = new SparkMax(14, SparkLowLevel.MotorType.kBrushless);
+                        }
+                        default -> throw new RuntimeException("Invalid module index");
+                }
+
+                angularOffset = Rotation2d.fromRadians(CHASSIS_ANGULAR_OFFSETS[index]);
+
+                drivingEncoder = driveSparkMax.getEncoder();
+                turningEncoder = turnSparkMax.getAbsoluteEncoder();
+
+                SparkMaxConfig driveConfig = new SparkMaxConfig();
+                SparkMaxConfig turnConfig = new SparkMaxConfig();
+
+                driveConfig.smartCurrentLimit(40).voltageCompensation(12.0).idleMode(SparkBaseConfig.IdleMode.kBrake);
+                // TODO: Is this correct?
+                driveConfig.encoder.quadratureMeasurementPeriod(10).quadratureAverageDepth(2)
+                                .positionConversionFactor(ModuleConstants.kDrivingEncoderPositionFactor)
+                                .velocityConversionFactor(ModuleConstants.kDrivingEncoderVelocityFactor);
+                driveConfig.signals.primaryEncoderPositionPeriodMs((int) (1000.0 / ODOMETRY_FREQUENCY));
+
+                turnConfig
+                                .inverted(isTurnMotorInverted)
+                                .smartCurrentLimit(30)
+                                .voltageCompensation(12.0)
+                                .idleMode(SparkBaseConfig.IdleMode.kBrake);
+                // TODO: Is this correct?
+                turnConfig.absoluteEncoder.averageDepth(2).inverted(ModuleConstants.kTurningEncoderInverted)
+                                .positionConversionFactor(ModuleConstants.kTurningEncoderPositionFactor)
+                                .velocityConversionFactor(ModuleConstants.kTurningEncoderVelocityFactor);
+                turnConfig.signals.absoluteEncoderPositionPeriodMs((int) (1000.0 / ODOMETRY_FREQUENCY));
+
+                driveSparkMax.configure(
+                                driveConfig, SparkBase.ResetMode.kResetSafeParameters,
+                                SparkBase.PersistMode.kPersistParameters);
+                turnSparkMax.configure(
+                                turnConfig, SparkBase.ResetMode.kResetSafeParameters,
+                                SparkBase.PersistMode.kPersistParameters);
+
+                this.drivePositionInput = OdometryThread.registerInput(drivingEncoder::getPosition);
+                this.steerRelativeEncoderPositionUngeared = OdometryThread.registerInput(turningEncoder::getPosition);
         }
 
-        drivingEncoder = driveSparkMax.getEncoder();
-        turningEncoder = turnSparkMax.getAbsoluteEncoder();
+        @Override
+        public void updateInputs(ModuleIOInputs inputs) {
+                inputs.driveWheelFinalRevolutions = drivingEncoder.getPosition() / DRIVE_GEAR_RATIO;
+                final double RPTO_REVOLUTIONS_PER_SECOND = 1.0 / 60.0;
+                inputs.driveWheelFinalVelocityRevolutionsPerSec = drivingEncoder.getVelocity() / DRIVE_GEAR_RATIO
+                                * RPTO_REVOLUTIONS_PER_SECOND;
 
-        drivingEncoder.setPositionConversionFactor(ModuleConstants.kDrivingEncoderPositionFactor);
-        drivingEncoder.setVelocityConversionFactor(ModuleConstants.kDrivingEncoderVelocityFactor);
+                inputs.driveMotorAppliedVolts = driveSparkMax.getAppliedOutput() * driveSparkMax.getBusVoltage();
+                inputs.driveMotorCurrentAmps = driveSparkMax.getOutputCurrent();
 
-        // Apply position and velocity conversion factors for the turning encoder. We
-        // want these in radians and radians per second to use with WPILib's swerve
-        // APIs.
-        turningEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderPositionFactor);
-        turningEncoder.setVelocityConversionFactor(ModuleConstants.kTurningEncoderVelocityFactor);
+                inputs.steerFacing = Rotation2d.fromRotations(turningEncoder.getPosition() / STEER_GEAR_RATIO)
+                                .minus(angularOffset);
+                inputs.steerVelocityRadPerSec = Units
+                                .rotationsPerMinuteToRadiansPerSecond(turningEncoder.getVelocity() / STEER_GEAR_RATIO);
 
-        // Invert the turning encoder, since the output shaft rotates in the opposite
-        // direction of
-        // the steering motor in the MAXSwerve Module.
-        turningEncoder.setInverted(ModuleConstants.kTurningEncoderInverted);
+                inputs.steerMotorAppliedVolts = turnSparkMax.getAppliedOutput() * turnSparkMax.getBusVoltage();
+                inputs.steerMotorCurrentAmps = turnSparkMax.getOutputCurrent();
 
-        SparkMaxConfig driveConfig = new SparkMaxConfig();
-        SparkMaxConfig turnConfig = new SparkMaxConfig();
+                inputs.odometryDriveWheelRevolutions = drivePositionInput.stream()
+                                .mapToDouble(value -> Units.rotationsToRadians(value) / DRIVE_GEAR_RATIO)
+                                .toArray();
+                drivePositionInput.clear();
+                inputs.odometrySteerPositions = steerRelativeEncoderPositionUngeared.stream()
+                                .map(value -> Rotation2d.fromRotations(value / STEER_GEAR_RATIO)
+                                                .minus(angularOffset))
+                                .toArray(Rotation2d[]::new);
+                steerRelativeEncoderPositionUngeared.clear();
+        }
 
-        driveConfig.smartCurrentLimit(40).voltageCompensation(12.0).idleMode(SparkBaseConfig.IdleMode.kBrake);
-        driveConfig.encoder.quadratureMeasurementPeriod(10).quadratureAverageDepth(2);
-        driveConfig.signals.primaryEncoderPositionPeriodMs((int) (1000.0 / ODOMETRY_FREQUENCY));
+        @Override
+        public void setDriveVoltage(double volts) {
+                driveSparkMax.set(volts / RobotController.getBatteryVoltage());
+        }
 
-        turnConfig
-                .inverted(isTurnMotorInverted)
-                .smartCurrentLimit(30)
-                .voltageCompensation(12.0)
-                .idleMode(SparkBaseConfig.IdleMode.kBrake);
-        turnConfig.encoder.quadratureMeasurementPeriod(10).quadratureAverageDepth(2);
-        driveConfig.signals.primaryEncoderPositionPeriodMs((int) (1000.0 / ODOMETRY_FREQUENCY));
-
-        driveSparkMax.configure(
-                driveConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        turnSparkMax.configure(
-                turnConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-
-        this.drivePositionInput = OdometryThread.registerInput(driveEncoder::getPosition);
-        this.steerRelativeEncoderPositionUngeared = OdometryThread.registerInput(steerRelativeEncoder::getPosition);
-    }
-
-    @Override
-    public void updateInputs(ModuleIOInputs inputs) {
-        inputs.driveWheelFinalRevolutions = driveEncoder.getPosition() / DRIVE_GEAR_RATIO;
-        final double RPTO_REVOLUTIONS_PER_SECOND = 1.0 / 60.0;
-        inputs.driveWheelFinalVelocityRevolutionsPerSec =
-                driveEncoder.getVelocity() / DRIVE_GEAR_RATIO * RPTO_REVOLUTIONS_PER_SECOND;
-
-        inputs.driveMotorAppliedVolts = driveSparkMax.getAppliedOutput() * driveSparkMax.getBusVoltage();
-        inputs.driveMotorCurrentAmps = driveSparkMax.getOutputCurrent();
-
-        inputs.steerFacing = Rotation2d.fromRotations(steerRelativeEncoder.getPosition() / STEER_GEAR_RATIO)
-                .minus(steerRelativePositionEncoderOffset);
-        inputs.steerVelocityRadPerSec =
-                Units.rotationsPerMinuteToRadiansPerSecond(steerRelativeEncoder.getVelocity() / STEER_GEAR_RATIO);
-
-        inputs.steerMotorAppliedVolts = turnSparkMax.getAppliedOutput() * turnSparkMax.getBusVoltage();
-        inputs.steerMotorCurrentAmps = turnSparkMax.getOutputCurrent();
-
-        inputs.odometryDriveWheelRevolutions = drivePositionInput.stream()
-                .mapToDouble(value -> Units.rotationsToRadians(value) / DRIVE_GEAR_RATIO)
-                .toArray();
-        drivePositionInput.clear();
-        inputs.odometrySteerPositions = steerRelativeEncoderPositionUngeared.stream()
-                .map(value ->
-                        Rotation2d.fromRotations(value / STEER_GEAR_RATIO).minus(steerRelativePositionEncoderOffset))
-                .toArray(Rotation2d[]::new);
-        steerRelativeEncoderPositionUngeared.clear();
-    }
-
-    private Rotation2d steerRelativePositionEncoderOffset = new Rotation2d();
-
-    @Override
-    public void calibrate() {
-        final Rotation2d steerActualFacing = Rotation2d.fromDegrees(turnSparkMax.get)
-                .minus(absoluteEncoderOffset);
-        final Rotation2d relativeEncoderReportedFacing =
-                Rotation2d.fromRotations(steerRelativeEncoder.getPosition() / STEER_GEAR_RATIO);
-        /* reported - offset = actual, so offset = reported - actual */
-        steerRelativePositionEncoderOffset = relativeEncoderReportedFacing.minus(steerActualFacing);
-    }
-
-    @Override
-    public void setDriveVoltage(double volts) {
-        driveSparkMax.set(volts / RobotController.getBatteryVoltage());
-    }
-
-    @Override
-    public void setSteerPowerPercent(double powerPercent) {
-        turnSparkMax.set(powerPercent);
-    }
+        @Override
+        public void setSteerPowerPercent(double powerPercent) {
+                turnSparkMax.set(powerPercent);
+        }
 }
