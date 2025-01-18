13b3aa4c0b6ab2b3a73fb757067b8a02a4f7e41b
Alex-idk <alex@alexidk.dev>
2025-01-18 12:38:21 -0500

    Added climber outline

---
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.climber;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase{
+    private final ClimberIO io;
+    private final ClimberIOInputsAutoLogged inputs = new ClimberIOInputsAutoLogged();
+
+    public Climber(ClimberIO io) {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Climber", inputs);
+    }
+
+    public Command setRadians(double radians) {
+        return null;
+    }
+}
